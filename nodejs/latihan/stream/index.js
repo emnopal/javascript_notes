9f32b168073923bbacca64c6e69a083f99633632
Muhammad Naufal <mnaufal0999@gmail.com>
2022-08-14 09:09:24 +0700

    tambahin beberapa tutor

---
+++ nodejs/latihan/stream/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const {resolve} = require('path');
+
+const absPath = resolve(__dirname); // get absolute path to project
+const input = `${absPath}/input.txt`
+const output = `${absPath}/output.txt`
+
+const readableStream = fs.createReadStream(input, {
+    highWaterMark: 15
+});
+
+const writableStream = fs.createWriteStream(output);
+
+// opening readable
+readableStream.on('readable', () => {
+    try {
+        writableStream.write(`${readableStream.read()}\n`);
+    } catch(ENOENT) {
+
+    }
+});
+
+// closing readable
+readableStream.on('end', () => writableStream.end());
+
